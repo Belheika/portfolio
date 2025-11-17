4ac1997e532ecfaf0eb857c74f118fbbb358f7bb
Beli <belinda-pro@outlook.com>
2025-11-17 17:53:19 +0000

    Initial portfolio commit

---
+++ animations.js
@@ -0,0 +1,25 @@
+function initStickers() {
+    const bookContainer = document.querySelector('.book-container');
+    const stickers = document.querySelectorAll('.sticker');
+    gsap.set(stickers, {
+        opacity: 0.5,
+        scale: 0.7,
+        zIndex: 1
+    });
+
+
+    bookContainer.addEventListener('mouseenter', () => {
+        gsap.to(stickers, {
+            x: (i) => [-472, 400, -480, 494, -480, 466, 200][i],
+            y: (i) => [-300, -300, 0, -40, 300, 300, -300][i],
+            rotation: (i) => [-3, 15, -3, 25, -15, 10, 20][i],
+            scale: 0.7,
+            opacity: 1,
+            duration: 1.2, // UN POIL PLUS RAPIDE
+            stagger: 0.2,
+            ease: "sine.inOut"
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', initStickers);
---
+++ script.js
@@ -0,0 +1,45 @@
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
+
+let panelsSection = document.querySelector("#panels"),
+    panelsContainer = document.querySelector("#panels-container"),
+    tween;
+
+document.querySelectorAll(".anchor").forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        let targetElem = document.querySelector(e.target.getAttribute("href")),
+            y = targetElem;
+        if (targetElem && panelsContainer.isSameNode(targetElem.parentElement)) {
+            let totalScroll = tween.scrollTrigger.end - tween.scrollTrigger.start,
+                totalMovement = (panels.length - 1) * targetElem.offsetWidth;
+            y = Math.round(tween.scrollTrigger.start + (targetElem.offsetLeft / totalMovement) * totalScroll);
+        }
+        gsap.to(window, {
+            scrollTo: {
+                y: y,
+                autoKill: false
+            },
+            duration: 1
+        });
+    });
+});
+
+const panels = gsap.utils.toArray("#panels-container .panel");
+tween = gsap.to(panels, {
+    xPercent: -100 * ( panels.length - 1 ),
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#panels-container",
+        pin: true,
+        start: "top top",
+        scrub: 0.5, // Réduit le scrub pour moins de sensibilité
+        snap: {
+            snapTo: 1 / (panels.length - 1),
+            inertia: true, // Active l'inertia pour plus de fluidité
+            duration: {min: 0.3, max: 0.8} // Durées plus longues
+        },
+        end: () =>  "+=" + (panelsContainer.offsetWidth - innerWidth)
+    }
+});
+
+
